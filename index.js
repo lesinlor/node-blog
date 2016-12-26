8429f854c39a7bb3ac77d05c04775e9f94b7bea4
lesinlor <287414993@qq.com>
2016-12-26 15:05:54 +0800

    add ejs
    Template

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+var path = require('path');
 var express = require('express');
 var app = express();
+var indexRouter = require('./controllers/index.js');
+var helloRouter = require('./controllers/hello.js');
 
-app.get('/',function(req,res){
-	res.send('hello express');
-})
+// app.get('/',function(req,res){
+// 	res.send('hello express');
+// })
 
-app.get('/users',function(req,res){
-	res.send('hello ' + req.query.name );
-})
+// app.get('/users',function(req,res){
+// 	res.send('hello ' + req.query.name );
+// })
+
+app.set('views',path.join(__dirname,'views'));//设置存放模板目录
+app.set('view engine','ejs');
+
+app.use('/',indexRouter);
+app.use('/',helloRouter);
 
 app.listen(3000,function(){
-	console.log("listen in port 3000")
+	console.log("listen in port 3000");
 });
