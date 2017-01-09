50f5097c307e16852cd1797409a8738bd2e2f8d6
lesinlor <287414993@qq.com>
2017-01-09 15:29:58 +0800

    1.0.0 beta
    publish

--- index.js
+++ index.js
@@ -1,23 +1,67 @@
 var path = require('path');
 var express = require('express');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
+var config = require('config-lite');
+var routes = require('./routes');
+var pkg = require('./package');
+
 var app = express();
-var indexRouter = require('./controllers/index.js');
-var helloRouter = require('./controllers/hello.js');
 
-// app.get('/',function(req,res){
-// 	res.send('hello express');
-// })
+//设置模板目录
+app.set('views', path.join(__dirname, 'views'));
 
-// app.get('/users',function(req,res){
-// 	res.send('hello ' + req.query.name );
-// })
+//设置模板引擎为 ejs
+app.set('view engine', 'ejs');
 
-app.set('views',path.join(__dirname,'views'));//设置存放模板目录
-app.set('view engine','ejs');
+//设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/',indexRouter);
-app.use('/',helloRouter);
+// session中间件
+app.use(session({
+    name: config.session.key,
+    secret: config.session.secret,
+    cookie: {
+        maxAge: config.session.maxAge
+    },
+    store: new MongoStore({
+        url: config.mongodb
+    })
+}));
 
-app.listen(3000,function(){
-	console.log("listen in port 3000");
+//flash中间件，用来显示通知
+app.use(flash());
+
+// 处理表单及文件上传的中间件
+app.use(require('express-formidable')({
+    uploadDir: path.join(__dirname, 'public/img'), // 上传文件目录
+    keepExtensions: true // 保留后缀
+}));
+
+// 设置模板全局常量
+app.locals.blog = {
+    title: pkg.name,
+    description: pkg.description
+};
+
+// 添加模板必需的三个变量
+app.use(function(req, res, next) {
+    res.locals.user = req.session.user;
+    res.locals.success = req.flash('success').toString();
+    res.locals.error = req.flash('error').toString();
+    next();
 });
+
+routes(app);
+
+// error page
+app.use(function(err, req, res, next) {
+    res.render('error', {
+        error: err
+    });
+});
+
+app.listen(config.port, function() {
+    console.log(`${pkg.name} listening on port ${config.port}`)
+})
---
+++ routes/signout.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+var checkLogin = require('../middlewares/check').checkLogin;
+
+// GET /signout 登出
+router.get('/', checkLogin, function(req, res, next) {
+	//清空session中用户信息
+	req.session.user = null;
+	req.flash('success','登出成功');
+	//登出后跳转到主页
+	res.redirect('/posts');
+    res.send(req.flash());
+});
+
+module.exports = router;
